89ed9055012587e149ea94cdb8b0fd9aa9f76b87
gowthamkrishnagk <43415128+gowthamkrishnagk@users.noreply.github.com>
2021-08-07 12:04:23 +0530

    code1 and 2 commit

---
+++ hello/pgm1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    m=(n++, ++n);
+    printf("%d",m);
+     
+}
---
+++ hello/pgm2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    n=5+n; 
+    printf("%c",n);
+    return 0;
+}
